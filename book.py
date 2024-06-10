40d6bff338d5b9bdffc169e65dc0e475e84dd45d
taooooo <791545998@qq.com>
2024-06-10 22:09:47 +0800

    [ADD] add countdown function

--- book.py
+++ book.py
@@ -4,7 +4,7 @@
 Author       : taooooo 791545998@qq.com
 Date         : 2024-05-16 22:45:57
 LastEditors  : taooooo 791545998@qq.com
-LastEditTime : 2024-05-24 21:47:19
+LastEditTime : 2024-06-10 22:04:28
 Description  : 
 
 '''
@@ -16,8 +16,8 @@ import threading
 from datetime import datetime, timedelta
 
 # 请将date_string、week替换为实际的值
-date_string = "2024-05-30"
-week = 4
+date_string = "2024-06-17"
+week = 1
 # 请将start_time和end_time替换为实际的时间段
 # start_time = "12:00"
 # end_time = "13:00"
@@ -26,8 +26,9 @@ time_slots = [("12:00", "13:00"), ("13:00", "14:00")]
 # 请将venue_id替换为实际的场地ID
 # venue_id = 8
 # 请将Authorization替换为实际的token
-Authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI4MzliZWJkNS1lMGQ0LTRiZGUtOWQ0YS0wNjM3N2YwYTc4NmUiLCJpYXQiOjE3MTY0NzU4MjAsImV4cCI6MTcxNjU2MjIyMCwidXNlckluZm8iOnsidXNlclJvbGVJZCI6MSwiYWNjb3VudCI6IjIwMjIyMTAxNzc5OCIsInNubyI6IjIwMjIyMTAxNzc5OCJ9LCJzZXgiOjEsInVzZXJJZCI6MjU5MjM0NTA1NzgzMzcsInNlcmlhbFZlcnNpb25VSUQiOi03NzAwODI1OTA0ODMwNzA0NjE5LCJpc0luaXRQYXNzd29yZCI6ZmFsc2UsInBob25lIjoiJHNpZ246cTl4djNBME4zMmtESkQyVVhueHlYZz09Iiwibmlja25hbWUiOiLljY7mhYjmtpsiLCJ0YWciOiJtaW5pIiwiaXNSZWFsTmFtZSI6dHJ1ZSwiYWNjb3VudCI6IiRzaWduOk1BN1krcFhGR0JDeEZaSlFKalNmQUE9PSJ9.H4O85QMC0X1LAFeStyN3o4IWRMx4WPjmO6CY8k0eFX5oHX2AR_jRJgKh2FslTlUQbi4bQx5W-0wsOIkgsVzz6ip2-_o1YAyLSwk_o2pa5IN0SWqFL5O3toBiN3isbGA9jPJ7ZhGS0rtYnbXuYxeLziP-JNAGrOdDPLX2aNtfvUZpVETAeZv4spLTTfDscx3DaaNHUeYC1UIB99ofrjeCADeWy7jDR1OxhHP2rPjrRKRA2V3skxfF3qTgv6JwEZ-zU0SJMdwr4bJMbhF6E_vqf9zcXkkQWgojiCffFtUGbBxAqne9L0O0BJtzcdTL9ufGKfhg0lYcRYQNY-29hZcrCg"
-
+Authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJhNWM5ZjkwMi1hZGNlLTQzZmMtOGU4Ny00NjlmOGFkNWYxYjUiLCJpYXQiOjE3MTc1OTYxNTgsImV4cCI6MTcxNzY4MjU1OCwidXNlckluZm8iOnsidXNlclJvbGVJZCI6MSwiYWNjb3VudCI6IjIwMjIyMTAxNzc5OCIsInNubyI6IjIwMjIyMTAxNzc5OCJ9LCJzZXgiOjEsInVzZXJJZCI6MjU5MjM0NTA1NzgzMzcsInNlcmlhbFZlcnNpb25VSUQiOi03NzAwODI1OTA0ODMwNzA0NjE5LCJpc0luaXRQYXNzd29yZCI6ZmFsc2UsInBob25lIjoiJHNpZ246cTl4djNBME4zMmtESkQyVVhueHlYZz09Iiwibmlja25hbWUiOiLljY7mhYjmtpsiLCJ0YWciOiJtaW5pIiwiaXNSZWFsTmFtZSI6dHJ1ZSwiYWNjb3VudCI6IiRzaWduOk1BN1krcFhGR0JDeEZaSlFKalNmQUE9PSJ9.Ml7ow_fafaJD5WfeEcWsmP4a9HyCXW1INiNUOHsvxA2Exoy8MYqqQuWraBanP4y3mf4d5s_nUKOSygujXlpnO7GTyuBbfx3_TTVB20xAxr4IKAdU1VaWOuxHNx_A9qqF5NbQM1LjXk-FLQVoJH0ipdDMq02WjSjl7-5Ai-W4wtQElxXa4qK3PjYT7EbOQU3L0-xOY7uP1SSf8T4a8cuSFPJZRxx-Rs0bga3eNtXR36R8LzJ12kwV6JfsV3o43CAXo2m_78fI6lbzlUQh7KU-T2RcDwtxzFLfzO1fRKDArBrRmlHf6BGfT7Xybpxf_ZSfVNZg0jUExZc0cNcny9KpkQ"
+# 停止信号
+should_stop = False
 
 # 场地的venueId
 venue_ids = {
@@ -76,7 +77,7 @@ def get_belong_date(date_string):
 
 
 def book_venue(start_time, end_time):
-    while True:
+    while not should_stop:
         for venue_id in venue_ids.keys():
             # 请求体
             payload = {
@@ -109,7 +110,7 @@ def book_venue(start_time, end_time):
                 print(response.text)
 
             # 等待0.1秒后再次尝试
-            time.sleep(0.1)
+            time.sleep(0.05)
         time.sleep(0.2)
 
 def threads_join(threads):
@@ -123,8 +124,18 @@ def threads_join(threads):
             else:
                 break
 
+def wait_until(target_time: datetime):
+    """等待直到目标时间"""
+    now = datetime.now()
+    wait_seconds = (target_time - now).total_seconds()
+    if wait_seconds > 0:
+        time.sleep(wait_seconds)
+
 
 if __name__ == "__main__":
+
+    target_time = datetime.now().replace(hour=23, minute=59, second=35, microsecond=0)
+    wait_until(target_time)
 
     # 创建线程列表
     threads = []
@@ -139,7 +150,10 @@ if __name__ == "__main__":
     for thread in threads:
         thread.start()
 
+    time.sleep(40)
+    should_stop = True  # 通知线程停止
+
     # 等待所有线程完成
-    threads_join(threads)
+    # threads_join(threads)
     for thread in threads:
         thread.join()
